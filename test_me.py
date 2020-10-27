1659c39eee1b12cedb553ec4bb533c0674869281
Marek Rychlik <rychlik@gmail.com>
2020-10-27 10:57:02 -0700

    *** empty log message ***

--- test_me.py
+++ test_me.py
@@ -1,10 +1,2 @@
-import datetime
-from count_readme_words import count_the_words
-
-def test_word_count():
-    run_name = datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
-    print(run_name)
-    count_the_words()
-
-if __name__ == '__main__':
-    test_word_count()
+import count_readme_words
+count_the_words()
