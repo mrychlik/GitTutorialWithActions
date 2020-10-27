1b28190fb05440826201671da8c01f874ad377a1
Marek Rychlik <rychlik@gmail.com>
2020-10-27 11:18:32 -0700

    *** empty log message ***

--- test_me.py
+++ test_me.py
@@ -1,7 +1,11 @@
 # This file will be located by pytest and functions will be executed
 import datetime
 
-def test_all():
+def test_datetime():
+    print(datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S'))
+
+
+def test_word_count():
     file = open("README.md", "rt")
     data = file.read()
     words = data.split()
