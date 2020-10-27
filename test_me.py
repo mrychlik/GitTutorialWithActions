5443657418cbab54b0d980c1eb3dbe0bf193308e
Marek Rychlik <rychlik@gmail.com>
2020-10-27 12:15:33 -0700

    *** empty log message ***

--- test_me.py
+++ test_me.py
@@ -1,11 +1,14 @@
 # This file will be located by pytest and functions will be executed
 import datetime
 
+
 def test_arithmetic():
     assert 2+2==4
 
+
 def test_faulty_arithmetic():
     assert 2+2==5
+
 
 def test_word_count():
     file = open("README.md", "rt")
